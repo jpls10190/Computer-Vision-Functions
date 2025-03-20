97c459ade4ebbdfbd92b5d0340e6e2201ffa1ad2
jpls10190 <jpls10190@gmail.com>
2025-03-20 15:06:06 +0000

    function

--- functions_CV.py
+++ functions_CV.py
@@ -2,11 +2,26 @@ from screeninfo import get_monitors
 import cv2
 
 # SHOW THE IMAGE IN THE CENTER OF THE SCREEN
-def imshow_center(img, window_name, monitor=0):
+def imshow_center(img_path, window_name, monitor=0):
+    img=cv2.imread(img_path)
+    if img is None:
+        raise ValueError("Error loading image.")
+
     # Get the primary monitor (first monitor)
     monitor = get_monitors()[monitor]
+
     # Get the window size (height, width)
     window_height, window_width = img.shape[:2]
+
+    # Compute the scaling factor to maintain aspect ratio
+    scale_factor = min(monitor.width / window_width, monitor.height / window_height)
+
+    # Compute new dimensions
+    new_width = int(window_width * scale_factor)
+    new_height = int(window_height * scale_factor)
+
+    # Resize the image
+    resized_image = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
     # Calculate the center of the screen
     center_x = monitor.width // 2
@@ -21,7 +36,7 @@ def imshow_center(img, window_name, monitor=0):
     cv2.moveWindow(window_name, top_left_x, top_left_y)  # Adjust window size accordingly
 
     # Display the object with the index
-    cv2.imshow(window_name, img)
+    cv2.imshow(window_name, resized_image)
 
 # RESIZE IMAGE 
 def resize_img(img, width, height):
